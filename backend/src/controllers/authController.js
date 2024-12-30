33076eece2c0c4d24d49fa1e6c318e1f57454fd2
sunny12100 <sunnygupta123420@gmail.com>
2024-12-30 14:35:46 +0530

    bug fix2

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { createUser, findUserByEmail } = require("../models/userModel.js");
 require("dotenv").config();
