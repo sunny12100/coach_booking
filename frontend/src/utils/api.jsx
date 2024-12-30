ebe2b11f00357a8a006f94da349cc4f4c1f93f5f
sunny12100 <sunnygupta123420@gmail.com>
2024-12-30 15:50:28 +0530

    bugfix3

--- frontend/src/utils/api.jsx
+++ frontend/src/utils/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000", // Replace with your backend URL
+  baseURL: "https://coach-booking-backend.onrender.com", // Replace with your backend URL
   headers: {
     "Content-Type": "application/json",
   },
